23e94b3f4e0ba3d56b55a36a890c6e9b5d134e33
ISXDora <isinhasena@gmail.com>
2021-07-24 16:14:52 -0300

    função abrir o modal ainda com problema

---
+++ Desafio 2.0/scripts.js
@@ -0,0 +1,18 @@
+const modalOverlay = document.querySelector('.modal-overlay');
+const cards =document.querySelectorAll('.card');
+
+    for (let card of cards ){
+        card.addEventListener("click", function(){
+            const pageId = card.getAttribute("id");
+            modalOverlay.classList.add('active')
+            modalOverlay.querySelector("iframe").src = `https://blog.rocketseat.com.br/${pageId}/`
+        })
+    }
+    
+    document.querySelector('.close-modal').addEventListener("click", function(){
+        modalOverlay.classList.remove('active')
+        modalOverlay.querySelector("iframe").src = ""
+        
+    })
+    
+
