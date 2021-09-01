a9ed683b72e104c432fa62ee7434f002adebaf8e
ISXDora <isinhasena@gmail.com>
2021-08-31 22:55:37 -0300

    commit initial - transition css and initial layouts njk

---
+++ modulo04/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const nunjucks = require('nunjucks');
+const routes = require("./routes");
+
+const server = express();
+
+server.set("view engine", "njk");
+
+server.use(express.static('public'))
+server.use(routes)
+
+nunjucks.configure("views", { 
+    express:server,
+    noCache: true,
+    autoespace: false
+});
+
+server.listen(5000, function () {
+    console.log('Server is running');
+});
