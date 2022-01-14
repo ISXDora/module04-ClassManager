d5ae606ca22de36338ff14ccabe2db8bea3d15bc
ISXDora <isinhasena@gmail.com>
2022-01-14 16:07:48 -0300

    configuring controller to insert data in db via post to teachers table

--- modulo05/src/app/controllers/teachers.js
+++ modulo05/src/app/controllers/teachers.js
@@ -1,12 +1,8 @@
 const {date, age} = require('../../lib/utils')
+const db = require('../../config/db')
 
 module.exports = {
-    index(req, res){
-        return res.render("teachers/index")
-    },
-    create(req, res){
-        return res.render('teachers/create')
-    },
+
     post(req, res){
         const keys = Object.keys(req.body)
     
@@ -16,8 +12,40 @@ module.exports = {
             }
     
         }
-        let {avatar_url, name, birth, schooling, modality, services} = req.body
-        return
+        
+        const query = `
+        INSERT INTO teachers (
+            avatar_url,
+            name,
+            birth_date,
+            education_level,
+            class_type,
+            subjects_taught,
+            created_at
+        ) VALUES ($1, $2, $3, $4, $5, $6, $7)
+        RETURNING id
+        `
+        const values = [
+            req.body.avatar_url,
+            req.body.name,
+            date(req.body.birth_date).iso,
+            req.body.education_level,
+            req.body.class_type,
+            req.body.subjects_taught,
+            date(Date.now()).iso
+        ]
+    
+        db.query(query, values, function(err, results){
+            if(err) return res.send("Database Error!")
+    
+            return res.redirect(`/teachers/${results.rows[0].id}`)
+        })
+    },
+    index(req, res){
+        return res.render("teachers/index")
+    },
+    create(req, res){
+        return res.render('teachers/create')
     },
     show(req, res){
         return
--- modulo05/src/config/db.js
+++ modulo05/src/config/db.js
@@ -1,9 +1,9 @@
 const { Pool } = require("pg")
 
 module.exports = new Pool ({
-    user: 'Postgres',
+    user: 'postgres',
     password: '123456',
     host: 'localhost',
-    database: 'myteacher',
+    database: 'my_teacher',
     port: 5433
 })
