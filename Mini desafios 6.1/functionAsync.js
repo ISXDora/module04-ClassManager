78f35065708699c2c604b76a9f22fe333f41eaf4
ISXDora <isinhasena@gmail.com>
2022-02-16 14:20:14 -0300

    refactor: nomeando funções e variavel

--- Mini desafios 6.1/functionAsync.js
+++ Mini desafios 6.1/functionAsync.js
@@ -1,23 +1,27 @@
-async function printDouble(number){
+function printDouble(number){
 
+  const ms = Math.floor(Math.random() * 100) + 1
+  
   return new Promise((resolve) => {
-    setTimeout( () => {
-     resolve(number * 2)
-   }, 
-   Math.floor(Math.random() * 100) + 1
-  )
+    setTimeout(() => {
+       resolve(number * 2)
+   },ms)
   })
       
   }
 
- printDouble(33).then(value => console.log(value))
+ function imprimeValor (value) {
+      console.log(value)
+ }
+
+ printDouble(33).then(imprimeValor)
 
 function printAll(){
-  printDouble(5).then(value => console.log(value))
-  printDouble(10).then(value => console.log(value))
-  printDouble(22).then(value => console.log(value))
-  printDouble(1).then(value => console.log(value))
-  printDouble(89).then(value => console.log(value))
+  printDouble(5).then(imprimeValor)
+  printDouble(10).then(imprimeValor)
+  printDouble(22).then(imprimeValor)
+  printDouble(1).then(imprimeValor)
+  printDouble(89).then(imprimeValor)
 }
 
- printAll()
+printAll()
